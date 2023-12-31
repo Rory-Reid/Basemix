8dcfadde2cd3d6fe1a4be2edbde165b515bf6165
Rory Reid <rory.roberts@outlook.com>
2023-12-31 18:09:36 +0000

    Setup iOS Distribution

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 dotnet publish src/Basemix/Basemix.csproj -c Release -f net7.0-windows10.0.19041.0 --no-restore
 dotnet publish src/Basemix/Basemix.csproj -c Release -f net7.0-maccatalyst -p:BuildIpa=True
 dotnet publish src/Basemix/Basemix.csproj -c Release -f net7.0-android --no-restore -p:AndroidKeyStore=True -p:AndroidSigningKeyPass="${ANDROID_SIGNING_KEY}" -p:AndroidSigningStorePass="${ANDROID_SIGNING_KEY}" -p:AndroidSigningKeyStore="${ANDROID_KEY_STORE}"
-dotnet build src/Basemix/Basemix.csproj -c Release -f net7.0-ios --no-restore /p:buildForSimulator=True /p:packageApp=True /p:ArchiveOnBuild=False
+dotnet publish src/Basemix/Basemix.csproj -c Release -f net7.0-ios -p:ArchiveOnBuild=true -p:RuntimeIdentifier=ios-arm64 -p:CodesignKey="${IOS_CODESIGN_KEY}" -p:CodesignProvision="${IOS_CODESIGN_PROVISION}"
